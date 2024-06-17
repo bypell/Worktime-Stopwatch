54d7fda9a782424b25b1b09955bbb8ba1cfd7a60
bypell <ippleac@gmail.com>
2024-06-17 13:21:42 -0400

    scrapped old c++ code + more documentation to gd code

--- addons/worktime_stopwatch/plugin.gd
+++ addons/worktime_stopwatch/plugin.gd
@@ -10,7 +10,7 @@ const CalendarDialog := preload("res://addons/worktime_stopwatch/calendar_dialog
 var saved_data_instance
 var main_widget_instance : Control
 var calendar_window_instance : Window
-var window_focused_notifier_instance : GlobalWindowFocusedNotifier
+#var window_focused_notifier_instance : GlobalWindowFocusedNotifier
 
 
 func _enter_tree() -> void:
@@ -20,11 +20,6 @@ func _enter_tree() -> void:
 	# instantiation
 	main_widget_instance = MainWidget.instantiate()
 	calendar_window_instance = CalendarDialog.instantiate()
-	
-	# window tracker
-	window_focused_notifier_instance = GlobalWindowFocusedNotifier.new()
-	window_focused_notifier_instance.start_notifying()
-	window_focused_notifier_instance.window_focused.connect(_new_window_focused)
 	
 	# signals
 	main_widget_instance.settings_menu_opening_requested.connect(_open_settings_menu)
@@ -42,7 +37,7 @@ func _enter_tree() -> void:
 
 
 func _exit_tree():
-	window_focused_notifier_instance.stop_notifying()
+	#window_focused_notifier_instance.stop_notifying()
 	_save_current_work_time()
 	
 	remove_control_from_docks(main_widget_instance)
@@ -99,11 +94,12 @@ func _open_settings_menu():
 	calendar_window_instance.popup_centered()
 
 
-# append previous_current_day_data to previous_days_data
-# and initialize data for new day, including day_number, date, work_time = 0, figuring out target_work_time, etc
+# appends data from the current (outdated) day to previous_days_data
+# and initializes data for a new day, including day_number, date, work_time = 0, figuring out target_work_time, etc.
 func _switch_to_new_day():
 	var current_date = Time.get_date_dict_from_system()
 	if saved_data_instance.current_day_data.date == current_date:
+		printerr("Tried to switch to a new day but the dates are the same.")
 		return
 	
 	saved_data_instance.previous_days_data.append(saved_data_instance.current_day_data)
@@ -123,8 +119,10 @@ func _switch_to_new_day():
 	#saved_data_instance.save_data()
 
 
+# Saves current progress and if the date is no longer the same, makes the necessary calls to switch to a new day
 func _save_current_work_time():
 	if not SavedData.verify_saved_data_exists():
+		push_warning("Couldn't save current work time. (data file not found)")
 		return
 	
 	saved_data_instance.current_day_data.work_time = main_widget_instance._elapsed_time
@@ -136,6 +134,7 @@ func _save_current_work_time():
 		_refresh_calendar_dialog()
 
 
+# Gets the difference in days between two date dictionaries
 func _get_days_diff_between_date_dicts(date_dict_1 : Dictionary, date_dict_2 : Dictionary) -> int:
 	var date_1 = Time.get_unix_time_from_datetime_dict(date_dict_1)
 	var date_2 = Time.get_unix_time_from_datetime_dict(date_dict_2)
@@ -143,24 +142,6 @@ func _get_days_diff_between_date_dicts(date_dict_1 : Dictionary, date_dict_2 : D
 	return (delta / 60 / 60 / 24)
 
 
+# Returns whether the current date (in the loaded saved data) is no longer the actual date
 func _is_current_date_newer_than_saved_current_date() -> bool:
 	return _get_days_diff_between_date_dicts(saved_data_instance.current_day_data.date, Time.get_date_dict_from_system()) > 0
-
-
-func _new_window_focused(window_title : String) -> void:
-	var title = window_title.to_lower().strip_edges()
-	if get_window().has_focus():
-		main_widget_instance._stopwatch_blocked = false
-		print("unblocked")
-		return
-	if calendar_window_instance.get_window().has_focus():
-		main_widget_instance._stopwatch_blocked = false
-		print("unblocked")
-		return
-	
-	if title.contains("blender"):
-		main_widget_instance._stopwatch_blocked = false
-		print("unblocked")
-	else:
-		main_widget_instance._stopwatch_blocked = true #TODO: change to function, since member is private
-		print("blocked")
